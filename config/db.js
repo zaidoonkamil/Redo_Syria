72094e326306725818841e38017e987028540484
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-06-16 13:51:23 +0300

    .

--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
 const { Sequelize } = require("sequelize");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "localhost",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: Number(process.env.DB_PORT || 3306),
     dialect: "mysql",
     logging: false,
-});
+  }
+);
 
 sequelize.authenticate()
-    .then(() => console.log("✅ Connected to MySQL successfully!"))
-    .catch(err => console.error("❌ Unable to connect to MySQL:", err));
-    
+  .then(() => console.log("✅ Connected to MySQL successfully!"))
+  .catch(err => console.error("❌ Unable to connect to MySQL:", err));
 
 module.exports = sequelize;
